6226537ba5115ef82f9a5834a20d0792bff29a3f
luiqor <vohnenna@gmail.com>
2025-01-12 16:26:01 +0200

    add vite config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { type ConfigEnv, defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const config = ({ mode }: ConfigEnv): ReturnType<typeof defineConfig> => {
+  const {
+    VITE_APP_API_ORIGIN_URL,
+    VITE_APP_DEVELOPMENT_PORT,
+    VITE_APP_PROXY_SERVER_URL,
+  } = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: Number(VITE_APP_DEVELOPMENT_PORT),
+      proxy: {
+        [VITE_APP_API_ORIGIN_URL as string]: {
+          changeOrigin: true,
+          target: VITE_APP_PROXY_SERVER_URL,
+        },
+      },
+    },
+  });
+};
+
+export default config;
