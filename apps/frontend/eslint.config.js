003ba6f94746e1d4fa87322ecc67743f1ed8a110
luiqor <vohnenna@gmail.com>
2025-01-12 16:45:06 +0200

    setup eslint

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,28 +1,29 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+import baseConfig from "../../eslint.config.js";
+
+/** @typedef {import("eslint").Linter.Config} */
+let Config;
+
+/** @type {Config} */
+const reactConfig = {
+  ignores: ["dist"],
+  files: ["**/*.{ts,tsx}"],
+  plugins: {
+    "react-hooks": reactHooks,
+    "react-refresh": reactRefresh,
   },
-)
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
+
+/** @type {Config[]} */
+const config = [...baseConfig, reactConfig];
+
+export default config;
---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+const config = [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
+
+export default config;
