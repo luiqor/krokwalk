5f1a2b5e17e876ac7ac1edbaf4f61ea80a3bd86e
luiqor <vohnenna@gmail.com>
2025-01-16 00:31:42 +0200

    refactor form styles

--- apps/frontend/src/libs/components/route-toolbar/route-toolbar.tsx
+++ apps/frontend/src/libs/components/route-toolbar/route-toolbar.tsx
@@ -1,4 +1,4 @@
-import { Divider, Button } from "@mui/material";
+import { Divider, Button, TextField } from "@mui/material";
 
 import { LocationSelection } from "../components.js";
 
@@ -7,20 +7,34 @@ import styles from "./route-toolbar.module.css";
 const RouteToolbar = () => {
   return (
     <div className={styles.toolbar}>
-      <form>
-        <LocationSelection />
-        <Divider />
-        <h3>Selected Tours</h3>
-        <p>Selected tours will be displayed here</p>
-        <Divider />
-        <h3>Selected Categories</h3>
-        <p>Selected categories will be displayed here</p>
-        <Divider />
-        <h3>Select settings</h3>
-        <p>Settings will be displayed here</p>
-        <Button type="submit" variant="contained" color="primary" fullWidth>
-          Submit
-        </Button>
+      <form className={styles.form}>
+        <div className={styles.locationSelectionContainer}>
+          <LocationSelection />
+        </div>
+        <div className={styles.settingsContainer}>
+          <div>
+            <Divider />
+            <h3>Selected Tours</h3>
+            <p>Selected tours will be displayed here</p>
+          </div>
+          <div>
+            <Divider />
+            <h3>Selected Categories</h3>
+            <p>Selected categories will be displayed here</p>
+          </div>
+          <div>
+            <Divider />
+            <h3>Select settings</h3>
+            <TextField
+              label="Provide maximum time for a walk"
+              size="small"
+              fullWidth
+            />
+          </div>
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Submit
+          </Button>
+        </div>
       </form>
     </div>
   );
