2bb64ab54a682b7648e82cb45108f11152a91b54
luiqor <vohnenna@gmail.com>
2025-01-15 14:38:51 +0200

    add places table migration

---
+++ apps/backend/src/db/migrations/20250115122644_add-places-table.ts
@@ -0,0 +1,43 @@
+import { type Knex } from "knex";
+
+const TABLE_NAME = "places";
+
+const ColumnName = {
+  ID: "id",
+  TITLE: "title",
+  DESCRIPTION: "description",
+  ADDRESS: "address",
+  PREVIEW: "preview",
+  LANTITUDE: "latitude",
+  LONGITUDE: "longitude",
+  ELEVATION: "elevation",
+  CREATED_AT: "created_at",
+  UPDATED_AT: "updated_at",
+} as const;
+
+function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.increments(ColumnName.ID).primary();
+    table.string(ColumnName.TITLE).notNullable();
+    table.string(ColumnName.DESCRIPTION).notNullable();
+    table.string(ColumnName.ADDRESS).notNullable();
+    table.string(ColumnName.PREVIEW).notNullable();
+    table.float(ColumnName.LANTITUDE).notNullable();
+    table.float(ColumnName.LONGITUDE).notNullable();
+    table.float(ColumnName.ELEVATION);
+    table
+      .dateTime(ColumnName.CREATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .dateTime(ColumnName.UPDATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  });
+}
+
+function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(TABLE_NAME);
+}
+
+export { up, down };
