a23009d61e51cb187dad6522c9df60abfce17e6e
luiqor <vohnenna@gmail.com>
2025-03-02 16:37:22 +0200

    add .env.example files

--- apps/backend/src/libs/modules/config/config.ts
+++ apps/backend/src/libs/modules/config/config.ts
@@ -12,9 +12,6 @@ const config = {
     DIALECT: process.env.DB_DIALECT,
     CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
   },
-  OSRM: {
-    URL: String(process.env.OSRM_URL),
-  },
   GEO_APIFY: {
     URL: String(process.env.GEO_APIFY_URL),
     API_KEY: String(process.env.GEO_APIFY_API_KEY),
