1236cf5317debc99688077c96db69ad2bf3c4d5c
luiqor <vohnenna@gmail.com>
2025-04-15 15:31:46 +0300

    add user to header in case of login

--- apps/frontend/src/libs/components/layout/libs/components/header/header.tsx
+++ apps/frontend/src/libs/components/layout/libs/components/header/header.tsx
@@ -2,17 +2,34 @@ import { Link } from "react-router-dom";
 import { AppRoute } from "~/libs/enums/enums.js";
 
 import styles from "./header.module.css";
+import { useAppSelector } from "~/libs/hooks/hooks.js";
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
-      <div>
-        <Link to={AppRoute.ROOT} className={styles.title}>
-          KrokWalk
-        </Link>
-      </div>
-    </header>
-  );
+	const { user } = useAppSelector((state) => state.auth);
+
+	if (!user) {
+		// get user
+	}
+
+	return (
+		<header className={styles.header}>
+			<div className={styles.container}>
+				<Link
+					to={AppRoute.ROOT}
+					className={styles.title}
+				>
+					KrokWalk
+				</Link>
+				<nav className={styles.navigation}>
+					{user ? (
+						`Welcome ${user.username}`
+					) : (
+						<Link to={AppRoute.SIGN_IN}>Login</Link>
+					)}
+				</nav>
+			</div>
+		</header>
+	);
 };
 
 export { Header };
