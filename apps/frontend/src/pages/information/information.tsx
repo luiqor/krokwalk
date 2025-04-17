02bb5d95d10a238ecc37f377899c3ff224cac164
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2025-04-17 16:49:10 +0300

    no implementation is required for the fixes

--- apps/frontend/src/pages/information/information.tsx
+++ apps/frontend/src/pages/information/information.tsx
@@ -1,22 +1,11 @@
-import { useTrackingGeolocationPermission } from "~/libs/components/popup-warning/libs/castome-hook/use-tracking-geolocation-permission.js";
-
-import {
-	InformationSection,
-	PopupWarning,
-} from "~/libs/components/components.js";
+import { InformationSection } from "~/libs/components/components.js";
 
 import styles from "./information.module.css";
 
 const Information = () => {
-	const geolocation = useTrackingGeolocationPermission();
-
 	return (
 		<div className={styles.container}>
 			<InformationSection />
-			<PopupWarning
-				isOpen={geolocation.isGeolocationEnabled}
-				onClose={() => geolocation.closedPopup()}
-			/>
 		</div>
 	);
 };
