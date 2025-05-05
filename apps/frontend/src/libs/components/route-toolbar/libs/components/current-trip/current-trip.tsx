a67f4061613f58b193a740e822c8636118bf621a
luiqor <vohnenna@gmail.com>
2025-05-05 22:01:12 +0300

    add complete trip button to auth user

--- apps/frontend/src/libs/components/route-toolbar/libs/components/current-trip/current-trip.tsx
+++ apps/frontend/src/libs/components/route-toolbar/libs/components/current-trip/current-trip.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { VisitStatus } from "shared";
 import { useAppDispatch, useAppSelector } from "~/libs/hooks/hooks.js";
-import { Button } from "@mui/material";
+import { Button, Tooltip } from "@mui/material";
 
 import { actions as locationAction } from "~/modules/location/location.js";
 import { actions as tripAction } from "~/modules/trips/trips.js";
@@ -13,13 +14,22 @@ const CurrentTrip: React.FC = () => {
 	const { startingPoint, destinationPoint, stopoverPoints, walkSeconds } =
 		useAppSelector((state) => state.trips);
 	const dispatch = useAppDispatch();
+	const { isAnonymousEnabled, user } = useAppSelector((state) => state.auth);
 
 	const handlePlanAnotherTrip = () => {
 		dispatch(locationAction.resetLocationData());
 		dispatch(tripAction.resetTripData());
 	};
 
+	const handleCompleteTrip = () => {
+		// TODO: Implement the logic to complete the trip
+	};
+
 	const { hours, minutes } = convertSecondsToHoursAndMinutes(walkSeconds ?? 0);
+
+	const areAllPointConfirmed = stopoverPoints.every(
+		(point) => point.visitStatus === VisitStatus.CONFIRMED
+	);
 
 	return (
 		<div className={styles.container}>
@@ -58,13 +68,37 @@ const CurrentTrip: React.FC = () => {
 					</div>
 				)}
 			</div>
-			<Button
-				variant="contained"
-				fullWidth
-				onClick={handlePlanAnotherTrip}
-			>
-				Plan Another Trip
-			</Button>
+			<div className={styles.buttonContainer}>
+				{(isAnonymousEnabled || user) && (
+					<Tooltip
+						title={
+							!areAllPointConfirmed
+								? "You need to confirm all stopover points to complete the trip."
+								: ""
+						}
+						placement="top"
+						arrow
+					>
+						<span>
+							<Button
+								variant="contained"
+								fullWidth
+								disabled={!areAllPointConfirmed}
+								onClick={handleCompleteTrip}
+							>
+								Complete Trip
+							</Button>
+						</span>
+					</Tooltip>
+				)}
+				<Button
+					variant="contained"
+					fullWidth
+					onClick={handlePlanAnotherTrip}
+				>
+					Plan Another Trip
+				</Button>
+			</div>
 		</div>
 	);
 };
