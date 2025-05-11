1d950d65aa0547019aeee82af07734a3d47d035a
luiqor <vohnenna@gmail.com>
2025-05-11 10:22:00 +0300

    remove redundant promise all in place repository

--- apps/backend/src/modules/places/place.repository.ts
+++ apps/backend/src/modules/places/place.repository.ts
@@ -43,22 +43,20 @@ class PlaceRepository implements Repository {
 
 		const places = await query;
 
-		return await Promise.all(
-			places.map(async (place) => {
-				return PlaceEntity.initialize({
-					id: place.id,
-					title: place.title,
-					description: place.description,
-					address: place.address,
-					thumbnailLink: place.thumbnailLink,
-					lat: place.lat,
-					lng: place.lng,
-					elevation: place.elevation ?? null,
-					createdAt: place.createdAt,
-					updatedAt: place.updatedAt,
-				});
-			})
-		);
+		return places.map((place) => {
+			return PlaceEntity.initialize({
+				id: place.id,
+				title: place.title,
+				description: place.description,
+				address: place.address,
+				thumbnailLink: place.thumbnailLink,
+				lat: place.lat,
+				lng: place.lng,
+				elevation: place.elevation ?? null,
+				createdAt: place.createdAt,
+				updatedAt: place.updatedAt,
+			});
+		});
 	}
 
 	public async getById(id: string): Promise<PlaceEntity | null> {
